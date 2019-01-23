60c4029b90925f55facf7cee4ba4fc7d8f7c7e18
Dave Goddard <dave@goddard.id.au>
2019-01-24 09:30:13 +1100

    Click to focus

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
+import 'dart:math';
 
 void main() => runApp(MyApp());
 
@@ -33,9 +34,12 @@ class _MyHomePageState extends State<MyHomePage> {
     Tuple2<String, List<int>>("Miles", [22, 33]),
     Tuple2<String, List<int>>("Ross", [])
   ];
+  var _focusedPlayerIdx = 0;
 
   @override
   Widget build(BuildContext context) {
+    var _maxScores = _scores.map((s) => s.item2.length).reduce(max);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -44,53 +48,62 @@ class _MyHomePageState extends State<MyHomePage> {
           child: GridView.builder(
         primary: false,
         padding: const EdgeInsets.all(20.0),
-        itemCount: (_scores.length *
-            (2 +
-                _scores.fold(
-                    0, (m, s) => s.item2.length > m ? s.item2.length : m))),
+        itemCount: (_scores.length * (2 + _maxScores)),
         gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: _scores.length, childAspectRatio: 2),
         itemBuilder: (BuildContext context, int index) {
-          final player = _scores[index % _scores.length];
+          final playerIdx = index % _scores.length;
+          final player = _scores[playerIdx];
           if (index < _scores.length) {
             var total = player.item2.fold(0, (t, v) => t + v);
             return Text(
-              "${player.item1}\n$total",
+              "${player.item1} (${player.item2.length})\n$total",
               textAlign: TextAlign.center,
               style: TextStyle(fontWeight: FontWeight.bold),
             );
           } else if (index < (_scores.length * 2)) {
-            final ctrl = TextEditingController();
-            var _focusNode = new FocusNode();
-            _focusNode.addListener(() {
-              if (!_focusNode.hasFocus) {
-                setState(() {
-                  if (ctrl.text != "") {
-                    final value = int.parse(ctrl.text);
-                    player.item2.insert(0, value);
-                  }
-                  _focusNode.unfocus();
-                  ctrl.clear();
-                });
-              }
-            });
-            return TextFormField(
-              autofocus: false,
-              keyboardType: TextInputType.number,
-              controller: ctrl,
-              textAlign: TextAlign.center,
-              textInputAction: TextInputAction.next,
-              focusNode: _focusNode,
-              decoration:
-                  InputDecoration(border: InputBorder.none, hintText: 'Score'),
-            );
+            if (playerIdx == _focusedPlayerIdx) {
+              final _ctrl = TextEditingController();
+              final _focusNode = FocusNode();
+              FocusScope.of(context).requestFocus(_focusNode);
+
+              return TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.number,
+                controller: _ctrl,
+                textAlign: TextAlign.center,
+                textInputAction: TextInputAction.next,
+                focusNode: _focusNode,
+                onFieldSubmitted: (value) {
+                  setState(() {
+                    if (value != "") {
+                      final player = _scores[_focusedPlayerIdx];
+                      player.item2.insert(0, int.parse(value));
+                    }
+                    _ctrl.clear();
+                    _focusedPlayerIdx = (_focusedPlayerIdx + 1) % _scores.length;
+                  });
+                },
+                decoration:
+                InputDecoration(border: InputBorder.none, hintText: 'Score'),
+              );
+            } else {
+              return GestureDetector(
+                  onTap: () {
+                  setState(() {
+                    _focusedPlayerIdx = playerIdx;
+                  });
+                },
+                  child: Text("S", textAlign: TextAlign.center),
+              );
+            }
           } else {
             var scoreIdx = (index / _scores.length).floor() - 2;
             if (scoreIdx < player.item2.length) {
               return Text("${player.item2[scoreIdx]}",
                   textAlign: TextAlign.center);
             } else {
-              return Text("N/A", textAlign: TextAlign.center);
+              return Text("", textAlign: TextAlign.center);
             }
           }
         },
