2f8a334b3f027fabbf95d658216ecbe6d6d91b64
Dave Goddard <dave@goddard.id.au>
2019-01-27 08:58:52 +1100

    Remove ability for out of order
    This means we can show rounds of the left, rather than per score

--- lib/scores.dart
+++ lib/scores.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tuple/tuple.dart';
-import 'dart:math';
 import 'package:math_expressions/math_expressions.dart';
 import 'keyboard.dart';
 
@@ -15,101 +13,59 @@ class ScoresPage extends StatefulWidget {
 }
 
 class _ScoresPageState extends State<ScoresPage> {
-  List<Tuple2<String, List<int>>> _scores;
-  var _focusedPlayerIdx = 0;
-
-  @override
-  initState() {
-    super.initState();
-    _scores = widget.players.map((name) => Tuple2(name, List<int>())).toList();
-  }
+  List<List<int>> _scores = [];
 
   addScore(String text) {
-    if (_focusedPlayerIdx >= 0 && text != "") {
-      final player = _scores[_focusedPlayerIdx];
+    var exp = Parser().parse(text);
+    var val = exp.evaluate(EvaluationType.REAL, new ContextModel()).floor();
 
-      Parser p = new Parser();
-      Expression exp = p.parse(text);
-
-      player.item2.insert(
-          0, exp.evaluate(EvaluationType.REAL, new ContextModel()).floor());
+    if (_scores.length == 0 || _scores[0].length == widget.players.length) {
+      _scores.insert(0, []);
     }
+    _scores[0].add(val);
   }
 
   @override
   Widget build(BuildContext context) {
-    var _maxScores = _scores.map((s) => s.item2.length).reduce(max);
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    var totals = widget.players.map((name) => 0).toList(growable: false);
+    _scores.forEach((round) => round.asMap().forEach((index, val) => totals[index] += val));
+
+    var playerIdx = (_scores.length > 0) ? _scores[0].length % widget.players.length : 0;
+    List<Widget> gridRows = [Text("")];
+    widget.players.asMap().forEach((index, name) => gridRows.add(Container(
+          color: playerIdx == index ? Colors.lightGreenAccent : null,
+          child: Center(child: Text("$name\n${totals[index]}",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold))),
+        )));
+
+    _scores.asMap().forEach((roundNum,round) {
+      gridRows.add(Center(child: Text((_scores.length - roundNum).toString())));
+      widget.players.asMap().forEach((i,name) =>
+          gridRows.add(Center(child: Text(
+              round.length > i ? round[i].toString() : ""
+          )))
+      );
+    });
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: Column(children: [
-        Expanded(
-            child: GridView.builder(
-          primary: false,
-          padding: const EdgeInsets.all(20.0),
-          itemCount: (_scores.length * (1 + _maxScores)),
-          gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: _scores.length, childAspectRatio: 2),
-          itemBuilder: (BuildContext context, int index) {
-            final playerIdx = index % _scores.length;
-            final player = _scores[playerIdx];
-            if (index < _scores.length) {
-              var total = player.item2.fold(0, (t, v) => t + v);
-              return GestureDetector(
-                  onTap: () => setState(() => _focusedPlayerIdx = playerIdx),
-                  child: Container(
-                      color: playerIdx == _focusedPlayerIdx
-                          ? Colors.lightGreenAccent
-                          : null,
-                      child: Text(
-                        "${player.item1}\n$total",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      )));
-            } else {
-              var scoreIdx = (index / _scores.length).floor() - 1;
-              if (scoreIdx < player.item2.length) {
-                final text =
-                    "${player.item2[scoreIdx]} (${player.item2.length - scoreIdx})";
-                return GestureDetector(
-                    onLongPress: () => showDialog(
-                        context: context,
-                        builder: (BuildContext context) => AlertDialog(
-                                title: Text(
-                                    "Delete ${text} from ${player.item1}?"),
-                                actions: <Widget>[
-                                  FlatButton(
-                                      child: const Text('DISAGREE'),
-                                      onPressed: () {
-                                        Navigator.pop(context, null);
-                                      }),
-                                  FlatButton(
-                                      child: const Text('AGREE'),
-                                      onPressed: () {
-                                        setState(() {
-                                          player.item2.removeAt(scoreIdx);
-                                        });
-                                        Navigator.pop(context, null);
-                                      })
-                                ])),
-                    child: Text(text, textAlign: TextAlign.center));
-              } else {
-                return Text("", textAlign: TextAlign.center);
-              }
-            }
-          },
-        )),
-        Keyboard(
-          onDone: (t) => setState(() {
-                addScore(t);
-                _focusedPlayerIdx = (_focusedPlayerIdx + 1) % _scores.length;
-              }),
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-        Text("\n"),
-      ])),
-    );
+        body: Column(children: [
+          Expanded(child: GridView.count(
+            crossAxisCount: widget.players.length + 1,
+            childAspectRatio: screenWidth / widget.players.length / 50,
+            children: gridRows,
+          )),
+          Keyboard(
+            onDone: (t) => setState(() {
+                  addScore(t);
+                }),
+          ),
+          Text("\n"),
+        ]));
   }
 }
