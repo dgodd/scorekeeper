5b5e0d5d661e25219cccb35d62e6a5d93b1a25dc
Dave Goddard <dave@goddard.id.au>
2019-01-27 09:14:05 +1100

    allow editing existing scores

--- lib/scores.dart
+++ lib/scores.dart
@@ -14,10 +14,19 @@ class ScoresPage extends StatefulWidget {
 
 class _ScoresPageState extends State<ScoresPage> {
   List<List<int>> _scores = [];
+  var editRound = -1;
+  var editPlayerIdx = -1;
 
   addScore(String text) {
     var exp = Parser().parse(text);
     var val = exp.evaluate(EvaluationType.REAL, new ContextModel()).floor();
+
+    if (editRound >= 0 && editPlayerIdx >= 0) {
+      _scores[editRound][editPlayerIdx] = val;
+      editRound = -1;
+      editPlayerIdx = -1;
+      return;
+    }
 
     if (_scores.length == 0 || _scores[0].length == widget.players.length) {
       _scores.insert(0, []);
@@ -30,24 +39,35 @@ class _ScoresPageState extends State<ScoresPage> {
     double screenWidth = MediaQuery.of(context).size.width;
 
     var totals = widget.players.map((name) => 0).toList(growable: false);
-    _scores.forEach((round) => round.asMap().forEach((index, val) => totals[index] += val));
+    _scores.forEach(
+        (round) => round.asMap().forEach((index, val) => totals[index] += val));
 
-    var playerIdx = (_scores.length > 0) ? _scores[0].length % widget.players.length : 0;
+    var playerIdx =
+        (_scores.length > 0) ? _scores[0].length % widget.players.length : 0;
     List<Widget> gridRows = [Text("")];
     widget.players.asMap().forEach((index, name) => gridRows.add(Container(
-          color: playerIdx == index ? Colors.lightGreenAccent : null,
-          child: Center(child: Text("$name\n${totals[index]}",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold))),
+          color: playerIdx == index && editRound == -1 && editPlayerIdx == -1 ? Colors.lightGreenAccent : null,
+          child: Center(
+              child: Text("$name\n${totals[index]}",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold))),
         )));
 
-    _scores.asMap().forEach((roundNum,round) {
+    _scores.asMap().forEach((roundNum, round) {
       gridRows.add(Center(child: Text((_scores.length - roundNum).toString())));
-      widget.players.asMap().forEach((i,name) =>
-          gridRows.add(Center(child: Text(
-              round.length > i ? round[i].toString() : ""
-          )))
-      );
+      widget.players.asMap().forEach((i, name) => gridRows.add(
+        GestureDetector(
+          child: Container(
+              color: editRound == roundNum && editPlayerIdx == i ? Colors.lightGreenAccent : null,
+              child: Center(child: Text(round.length > i ? round[i].toString() : ""))
+          ),
+          onTap: () =>
+            setState(() {
+              editRound = roundNum;
+              editPlayerIdx = i;
+            })
+        ),
+      ));
     });
 
     return Scaffold(
@@ -55,7 +75,8 @@ class _ScoresPageState extends State<ScoresPage> {
           title: Text(widget.title),
         ),
         body: Column(children: [
-          Expanded(child: GridView.count(
+          Expanded(
+              child: GridView.count(
             crossAxisCount: widget.players.length + 1,
             childAspectRatio: screenWidth / widget.players.length / 50,
             children: gridRows,
